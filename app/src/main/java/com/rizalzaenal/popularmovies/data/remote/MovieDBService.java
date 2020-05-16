6895c4f889960023fb15cc1186571acfdf839997
rizal zaenal <rizaenalofficial@gmail.com>
2020-05-16 07:26:00 +0700

    add query to service interface

--- app/src/main/java/com/rizalzaenal/popularmovies/data/remote/MovieDBService.java
+++ app/src/main/java/com/rizalzaenal/popularmovies/data/remote/MovieDBService.java
@@ -3,12 +3,13 @@ package com.rizalzaenal.popularmovies.data.remote;
 import com.rizalzaenal.popularmovies.data.model.ResultInfo;
 import io.reactivex.Single;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface MovieDBService {
 
   @GET("movie/popular")
-  Single<ResultInfo> getPopularMovies();
+  Single<ResultInfo> getPopularMovies(@Query("api_key") String apiKey);
 
   @GET("movie/top_rated")
-  Single<ResultInfo> getTopRatedMovies();
+  Single<ResultInfo> getTopRatedMovies(@Query("api_key") String apiKey);
 }
